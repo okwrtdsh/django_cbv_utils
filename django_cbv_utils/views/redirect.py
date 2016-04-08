59a88074dac20782637882b2271d9e90651a1f6c
YoKW <okwrtdsh@gmail.com>
2016-04-08 10:25:59 +0900

    redirectにGETパラメータを渡せるように

---
+++ django_cbv_utils/views/redirect.py
@@ -0,0 +1,15 @@
+from urllib.parse import urlencode
+from django.http.response import HttpResponseRedirect
+
+
+class RedirectWithParamsMixin(object):
+    params = {}
+
+    def get_params(self, form=None):
+        return self.params
+
+    def form_valid(self, form):
+        super(RedirectWithParamsMixin, self).form_valid(form)
+        return HttpResponseRedirect(
+            self.get_success_url() + "?%s" % urlencode(self.get_params(form)))
+
