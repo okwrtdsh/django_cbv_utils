7138fe6539d141d81a7feb3caad76be7fe0c3faa
okwrtdsh <okwrtdsh@gmail.com>
2016-08-23 14:50:57 +0900

    __init__.py に追加

--- django_cbv_utils/views/__init__.py
+++ django_cbv_utils/views/__init__.py
@@ -2,4 +2,5 @@ from .search import SearchListView
 from .json import JSONResponseMixin, JSONView, FormJSONResponseView,\
      HybridJSONResponseMixin
 from .event import GETBindEventMixin, POSTBindEventMixin
+from .redirect import RedirectWithParamsMixin
 
